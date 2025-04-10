ce4248804a9f1b9c815c7e4bc7d7b2df55bcbbde
pedrosh <serdiopedro@gmail.com>
2025-04-10 12:39:10 -0700

    Calculate and display total feedback, average, and percent positive in the feedback app

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,7 +5,10 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
+  const total = good + bad + neutral
+  const avg = (good - bad) / total
+  const percentPos = (good / total) * 100
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -17,7 +20,11 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
-
+      <br />
+      
+      <p>total: {total}</p>
+      <p>average: {avg}</p>
+      <p>percent positive: {percentPos} % </p>
     </div>
   )
 }
